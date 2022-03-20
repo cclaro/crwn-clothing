ae29f812736d20d536fe9cc8829b2a77df5f5f17
Carlos Claro <carlos@linktr.ee>
2022-03-20 13:57:26 +1100

    added our sing in component as weel as firebase uitls

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,60 @@
+import { async } from '@firebase/util';
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAY3b4cL9TTBbrG5rYOwXPzVSRq8dUiPcw",
+  authDomain: "crwn-clothing-db-e441e.firebaseapp.com",
+  projectId: "crwn-clothing-db-e441e",
+  storageBucket: "crwn-clothing-db-e441e.appspot.com",
+  messagingSenderId: "533803053143",
+  appId: "1:533803053143:web:2e9ec728069f5d84413312"
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+  prompt: "select_account"
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid )
+
+  console.log(userDocRef)
+
+  const userSnapshot = await getDoc(userDocRef)
+  console.log(userSnapshot)
+  console.log(userSnapshot.exists)
+
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createAt = new Date()
+
+    try {
+      await setDoc( userDocRef, {
+        displayName,
+        email,
+        createAt
+      })
+    } catch (error) {
+      console.log('error creating the user', error.message)
+    }
+
+    return userDocRef
+
+  }
+
+
+  // if user does data exists
+  // if user data exists
+
+}
